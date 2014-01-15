4d1e04ce76ff97c320fd5f07fc9e282ade7fa352
Ivar Eriksen <ivar@flyndre.com>
2014-01-15 11:03:24 +0100

    added go file

---
+++ Ex 1/ex1.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    . "fmt"     // Using '.' to avoid prefixing functions with their package names
+    . "runtime" //   This is probably not a good idea for large projects...
+    . "time"
+)
+
+var i = 0
+
+func adder() {
+    for x := 0; x < 1000000; x++ {
+        i++
+    }
+}
+
+func subtract() {
+     for x := 0; x < 1000000; x++ {
+        i--
+    }
+}
+
+func main() {
+    GOMAXPROCS(NumCPU())        // I guess this is a hint to what GOMAXPROCS does...
+    go adder()                  // This spawns adder() as a goroutine
+    go subtract()
+    for x := 0; x < 50; x++ {
+        Println(i)
+    }
+    // No way to wait for the completion of a goroutine (without additional syncronization)
+    // We'll come back to using channels in Exercise 2. For now: Sleep
+    Sleep(100*Millisecond)
+    Println("Done:", i);
+}
