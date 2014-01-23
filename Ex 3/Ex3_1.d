9d314b3d731c01322db818608fed881ce8e785c4
Ivar Eriksen <ivar@flyndre.com>
2014-01-23 11:35:31 +0100

    oppg 1 endelig

--- Ex 3/Ex3_1.d
+++ Ex 3/Ex3_1.d
@@ -1,15 +1,30 @@
-import std.stdio
-import std.socket
+import std.stdio;
+import std.socket;
+import std.string;
 
-int main() {
+void main() {
   
   const int PORT = 34933;
+  string ip = "129.241.187.161";
   
-  InternetAddress addr = new InternetAddress(/*server ip*/, PORT);
-  TcpSocket sock = new socket(AdressFamily.INET, SocketType.STREAM, ProtocolType.TCP);
+  InternetAddress addr = new InternetAddress(ip, PORT);
+  Socket sock = new TcpSocket(addr);
+  scope(exit) sock.close();
   
-  sock.connect(addr); //connectTCP() ??
-
-
-
+  
+  
+  char [1024] buffer;
+  auto recv = sock.receive(buffer);
+  writefln("received: %s", buffer);
+  
+  for(int i = 0; i < 3; i++){
+    char [] message;
+    writeln("Write message:");
+    readln(message);
+    message[message.length-1] = '\0';
+    sock.send(message);
+    sock.receive(buffer);
+    writeln(buffer,"\n");
+    
+  }
 }
