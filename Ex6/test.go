88e6a5cc230ea394bc3badacd07fbacf92393c4f
Ivar Eriksen <ivar@flyndre.com>
2014-03-06 02:14:26 +0100

    Ex6 done

--- Ex6/test.go
+++ Ex6/test.go
@@ -6,30 +6,35 @@ import (
 	"flag"
 	"time"
 	"os/exec"
-	"os/signal"
-	"syscall"
 	"log"
+	"net"
+	"strconv"
 )
 
 var backupflag = flag.Bool("backup", false, "Start as backup process")
+var countflag = flag.Int("count", 0, "Where to start couting")
 
 func main() {
 	flag.Parse();
-	fmt.Println("Backupflag is set to:", *backupflag)
 	if *backupflag {
-		backup()
+		backup(*countflag)
 	} else {
-		spawnBackup()
-		mainLoop()
+		spawnBackup(0)
+		mainLoop(0)
 	}
 }
 
-func mainLoop() {
-	count()
+func mainLoop(count int) {
+	number := make(chan int)
+	time.Sleep(200 * time.Millisecond)
+	go netSend(number)
+	counter(number, count)
 }
-func spawnBackup() {
-	fmt.Println("Spawning backup process")
-	cmd := exec.Command("./test", "--backup")
+
+func spawnBackup(count int) {
+	log.Println("Spawning new backup process")
+	cntr := "-count=" + strconv.Itoa(count)
+	cmd := exec.Command("./test", "-backup", cntr)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	err := cmd.Start()
@@ -38,29 +43,94 @@ func spawnBackup() {
 	}
 }
 
-func backup() {
-	fmt.Println("Backup started");
-	ch := make(chan os.Signal, 1)
-        signal.Notify(ch, syscall.SIGUSR1)
+func netSend(number chan int) {
+	serverAddr, err := net.ResolveUDPAddr("udp", "localhost:9999")
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	con, err := net.DialUDP("udp", nil, serverAddr)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	for {
+		num :=  <-number
+		buf := []byte(strconv.Itoa(num))
+		_, err := con.Write(buf)
+		if err != nil {
+			log.Fatal("error writing data", err)
+		}
+	}
+}
+
+func netBackup(number chan int, quit chan bool) {
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:9999")
+	if err != nil {
+		log.Fatal("Error finding address: ", err)
+	}
+	netlisten, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatal("Error opening port:", err);
+	}
+	buffer := make([]byte, 1024)
+	oob := make([]byte, 1024)
+	defer netlisten.Close()
+	for {
+		select{
+		case <-quit:
+			log.Println("Listener shutting down")
+			return
+		default:
+			netlisten.SetDeadline(time.Now().Add(1e9))
+			n, _, _, _, err := netlisten.ReadMsgUDP(buffer, oob)
+			if err != nil {
+				if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+					continue
+				} else {
+					log.Fatal("Error:", err)
+				}
+			} else {
+				num, err := strconv.Atoi(string(buffer[:n]))
+				if err != nil {
+					log.Fatal("Error casting buffer to int: ", err)
+				}
+				number <- num
+			}
+		}
+	}
+}
+
+func backup(count int) {
+	number := make(chan int)
+	quit := make(chan bool)
+	go netBackup(number, quit)
+	missed_packet := 0
+	var num int
 loop:
 	for {
 		select {
-		case signal := <-ch:
-			log.Println("Got signal, ", signal)
-			break loop
+		case bla := <-number:
+			missed_packet = 0
+			num = bla
+			time.Sleep(200 * time.Millisecond)
 		default:
-			log.Println("No signal, sleeping")
+			if missed_packet == 3 {
+				quit <- true
+				spawnBackup(num+1)
+				mainLoop(num+1)
+				break loop
+			}
+			missed_packet += 1
 			time.Sleep(200 * time.Millisecond)
 		}
 	}
-	fmt.Println("Main proccess died");
-	spawnBackup()
-	mainLoop()
 }
 
-func count() {
-	for i := 0; i <= 5; i++ {
+func counter(number chan int, count int) {
+	i := count
+	for  {
 		fmt.Println(i)
+		number <-i
 		time.Sleep(200 * time.Millisecond)
+		i++
 	}
 }
