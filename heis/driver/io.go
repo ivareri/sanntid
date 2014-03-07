744ce81a6c780afcf8478ccdfd2bb414bbfa1eeb
Ivar Eriksen <ivar@flyndre.com>
2014-03-07 19:50:05 +0100

    meh

--- heis/driver/io.go
+++ heis/driver/io.go
@@ -1,88 +1,104 @@
 package io
-/*
-CFLAGS = -std=c99 -g -Wall -O2 -I . -MMD
-LDFLAGS = -lpthread -lcomedi -g -lm
-#include "io.h"
-*/
-import "C"
-import "log"
 
+import (
+	"log"
+)
 
-/**
-  Initialize libComedi in "Sanntidssalen"
-  @return Non-zero on success and 0 on failure
-*/
+type buttonType int
+const (
+	up Button = itoa+1
+	down
+	command
+	stop
+	obstruction
+)
 
-
-func io_init() bool {
-	n, err := C.io_init()
-	if err != nil {
-		log.Fatal("Error interfacing C driver: ", err)
-	}
-	return bool(n)
+type floorButton struct {
+	floor int
+	button buttonType
 }
 
+func init(void) {
+	// Init hardware
+	if (!io_init()) {
+		log.Fatal("Error during HW init")
+	}
 
-/**
-  Sets a digital channel bit.
-  @param channel Channel bit to set.
-*/
-func io_set_bit(channel int) {
-	_, err := C.io_set_bit(C.int(channel))
-	if err != nil {
-		log.Fatal("Error interfacing C driver: ", err)
+	// Zero all floor button lamps
+  // Clear stop lamp, door open lamp, and set floor indicator to ground floor.
+  // Return success.
+    return true;
+}
+
+func runMotor(speed uint, direction bool) {
+
+	// Invert direction in order to break elevator before stopping
+	if speed == 0 {
+		direction = !direction
+	}
+	if direction {
+		io_set_bit(MOTORDIR)
+	} else {
+		io_clear_bit(MOTORDIR)
+	}
+	io_write_analog(MOTOR, 2048 + 4 * speed)
+}
+
+func readFloorsensor(floor chan uint) {
+	currenFloor := -1;
+	if io_read_bit(SENSOR1) && ( currentFloor != 1) {
+		setFloorLight(1)
+		floor <- 1
+	} else if io_read_bit(SENSOR2) && (currentFloor != 2) {
+		setFloorLight(2)
+		floor <- 2
+	} else if io_read_bit(SENSOR3) && (currentFloor != 3) {
+		setFloorLight(3)
+		floor <- 3
+	} else if io_read_bit(SENSOR4) && (currentFloor != 4) {
+		setFloorLight(4)
+		floor <- 4
+	} else if currentFloor != 0 {
+		floor <- 0
 	}
 }
 
-/**
-  Clears a digital channel bit.
-  @param channel Channel bit to set.
-*/
-func io_clear_bit(channel int) {
-	_, err := C.io_clear_bit(C.int(channel))
-	if err != nil {
-		log.Fatal("Error interfacing C driver: ", err)
+func setFloorLight(floor int) {
+	if (floor < 1) || (floor > 4) {
+		log.Fatal("Floor out of range: ", floor)
+	}
+	switch floor {
+	case 1:
+		io_clear_bit(FLOOR_IND1)
+		io_clear_bit(FLOOR_IND2)
+	case 2:
+		io_set_bit(FLOOR_IND1)
+		io_clear_bit(FLOOR_IND2)
+	case 3:
+		io_clear_bit(FLOOR_IND1)
+		io_set_bit(FLOOR_IND2)
+	case 4:
+		io_set_bit(FLOOR_IND1)
+		io_set_bit(FLOOR_IND2)
 	}
 }
 
-
-/**
-  Writes a value to an analog channel.
-  @param channel Channel to write to.
-  @param value Value to write.
-*/
-func io_write_analog(channel, value int) {
-	_, err := C.io_write_analog(C.int(channel), C.int(value))
-	if err != nil {
-		log.Fatal("Error interfacing C driver: ", err)
-	}
+func setLight() {
+ // write some fancy code
 }
 
-
-/**
-  Reads a bit value from a digital channel.
-  @param channel Channel to read from.
-  @return Value read.
-*/
-func io_read_bit(channel int) bool {
-	n, err := C.io_read_bit(C.int(channel))
-	if err != nil {
-		log.Fatal("Error interfacing C driver: ", err)
-	}
-	return bool(n)
+func emergencyStop(bool stop) {
+	if stop {
+			io_set_bit(LIGHT_STOP)
+		} else {
+			io_clear_bit(LIGHT_STOP)
+		}
 }
 
-
-
-/**
-  Reads a bit value from an analog channel.
-  @param channel Channel to read from.
-  @return Value read.
-*/
-func io_read_analog(channel int) {
-	n, err := C.io_read_analog(C.int(channel)) 
-	if err != nil {
-		log.Fatal("Error interfacing C driver: ", err)
+func doorOpen(bool open) {
+	if open {
+		io_set_bit(DOOR_OPEN);
+	} else {
+		io_clear_bit(DOOR_OPEN);
 	}
-	return int(n)
 }
