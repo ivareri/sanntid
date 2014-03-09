5e00d41abf6f48679cfc4c51c5eb852f5249cba5
Inger Berge Hagen <ingerbergehagen@dhcp-008118.wlan.ntnu.no>
2014-03-09 17:59:03 +0100

    not finished either

--- heis/elevator.go
+++ heis/elevator.go
@@ -6,13 +6,17 @@ import (
 )
 
 func elevatorControl(){
-	
 	floorOrder := make(chan uint)
 	buttonPress := make(chan Button)
 	floor := make(chan FloorStatus)
 	
-	init(floorOrder, floor)
-	go readButtons(buttonPress) // puts pressed buttons in the buttonPress chanel
+	// [floor][0]=up, [floor][1]=down, [floor][2]= command 
+	// localqueue[4][3] bool
 	
+	init(floorOrder, floor)
+	go ReadButtons(buttonPress) // puts pressed buttons in the buttonPress chanel
+	go GetOrder(floorOrder)
+	
+	// func that gets something from manager and runs the different localQueue funcs
 	
 }
