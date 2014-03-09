b68092b97cea5c56304ce1ddc9edf51ca01097e5
Inger Berge Hagen <ingerbergehagen@dhcp-008118.wlan.ntnu.no>
2014-03-09 17:28:31 +0100

    comments

--- heis/elevator.go
+++ heis/elevator.go
@@ -3,6 +3,15 @@ import (
 	"io"
 	"localQueue"
 )
+
 func elevatorControl(){
+	floorOrder := make(chan uint)
+	buttonPress := make(chan Button)
+	floor := make(chan FloorStatus)
+	
+	init(floorOrder, floor)
+
+	go readButtons(buttonPress) // puts pressed buttons in the buttonPress chanel
+	
 	
 }
