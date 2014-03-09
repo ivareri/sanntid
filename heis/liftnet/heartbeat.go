a0a3d139afbcc53aacc8a0e447e91e98c29cd7ad
Ivar Eriksen <ivar@flyndre.com>
2014-03-09 22:03:31 +0100

    did stuff

--- heis/liftnet/heartbeat.go
+++ heis/liftnet/heartbeat.go
@@ -1,39 +1,84 @@
 package liftnet
 
 import (
-	"net"
-	"log"
-	"os"
-//	"time"
-	"strings"
-	"strconv"
 	"errors"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
+const bcastPort = 63000
 
-
-func FindIP() (string, error) {
-	name, err := os.Hostname()
+func sendStatus(statusch chan string, stopUDPBroadcast chan bool) {
+	broadcast, err := net.ResolceUDPAddr("udp", IPv4bcast+":"+bcastPort)
 	if err != nil {
-		return "", err
+		log.Fatal("Fatal error:", err)
 	}
-
-	addrs, err := net.LookupHost(name)
+	con, err := net.DialUDP("udp", nil, broadcast)
 	if err != nil {
-		return "", err
+		log.Fatal("Fatal error:", err)
 	}
-	for _, a := range addrs {
-		if strings.Contains(a,".") {
-			return a, nil
+	defer con.Close()
+	for {
+		select {
+		case <-stopUDPBroadcast:
+			return
+		case status <- statusch:
+			enc, err := json.Marshal(status)
+			if err != nil {
+				log.Println("Error: ", err)
+			} else {
+				encoded = en
+			}
+		default:
+			_, err := con.Write(encoded)
+			if err != nil {
+				log.Println("Error sending status:", err)
+			}
+			time.Sleep(500 * time.Millisecond)
 		}
 	}
-	return "", errors.New("Unable to find IPv4 address")
 }
 
-func FindID(a string) int {
-	id, err := strconv.Atoi(strings.Split(a ,".") [3])
+func listenStatus(closeUDPListen chan bool, statuschan chan string) {
+	udpAddr, err := net.ResolveUDPAddr("udp", IPv4addr+":"+bcastPort)
 	if err != nil {
-		log.Fatal("Error converting IP to ID", err)
+		log.Fatal("Error: ", err)
 	}
-	return id
+	netlisten, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	defer netlisten.Close()
+	buffer, oob := make([]byte, 1024)
+	for {
+		netlisten.SetDeadline(time.Now().Add(1e9))
+		n, _, addr, _, err := netlisten.ReadMsgUDP(buffer, oob)
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
+				continue
+			} else {
+				log.Fatal("Error:", err)
+			}
+		} else {
+			status, _ := unpackUDP(addr, buffer)
+			statuschan <- status
+		}
+		switch {
+		case <-closeUDPListen:
+			return
+		}
+	}
+}
+
+func unpackUDP(addr net.IPAddr, buffer []byte) (string, error) {
+
+	status, err := json.Unmarshal(buffer[:n])
+	if err != nil {
+		log.Println("Error unmarshaling: ", err)
+	}
+
 }
