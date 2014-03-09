12b885657967e2b5b02505a1d322ee951d938822
Inger Berge Hagen <ingerbergehagen@dhcp-008118.wlan.ntnu.no>
2014-03-09 17:28:31 +0100

    comments

--- heis/localQueue.go
+++ heis/localQueue.go
@@ -1,13 +1,51 @@
 package localQueue
+import (
+	"log"
+)
+
+// Adds floor to local Queue and tells network
+func addLocalCommand(buttonPressed button, localQueue [][]bool){
+	SetLight(Light{buttonPressed.Floor, Command, true})
+	localQueue[floor-1][Command] = true 
+	//TODO: Tell network to addCommand(floor, lift)
+}
+
+// Deletes floor from local Queue and tells network
+func deleteLocalCommand(floor uint ,localQueue [][]bool){
+	SetLight(Light{floor, Command, false})
+	localQueue[floor-1][Command] = false
+	//TODO: Tell network to deleteCommand(floor, lift)
+}
+
+// Adds request to localQueue
+func addLocalRequest(manager chan button, localQueue [][]bool){
+	buttonPressed := <- manager
+	SetLight(Light{buttonPressed.Floor, buttonPressed.Button, true})
+	localQueue[buttonPressed.Floor-1][buttonPressed.Button-1] = true
+}
+
+// Deletes requests from localQueue
+func deleteLocalRequest(Direction bool, floor uint, localQueue [][]bool){
+	if Direction {
+		SetLights(Light{floor, Up, false})
+		localQueue[floor-1][Up-1] = false
+		//TODO: Tell network
+	}else{
+		SetLights(Light{floor, Down, false})
+		localQueue[floor-1][Down-1] = false
+		//TODO: Tell network
+	}
+}
+
 // Returns next floor ordered from the local queue or 0 if empty
-func getOrder(status chan struct,localQueue [][]bool) int {
+func getOrder(floorOrder chan uint status chan struct, localQueue [][]bool) int {
 	status := <-status
 	currentFloor := status.floor
 	currentIndex = int(currentFloor-1)
 	
 	if status.direction {
 		if next := checkUp(currentIndex , 3, localQueue); next && currentIndex !=3 {
-			return next
+			floorOrder
 		}else if next := checkDown(3, 0, localQueue); next{
 			return next
 		}else{
@@ -29,7 +67,7 @@ func getOrder(status chan struct,localQueue [][]bool) int {
 // Returns next floor ordered above current in UP queue or 0 if empty
 func checkUp(start int, stop int, lockalQueue [][]bool) int {
 	for i := floor; i <= stop ; i++ {
-		if localQueue[i][1] || localQueue[i][2]{
+		if localQueue[i][Up-1] || localQueue[i][Command-1]{
 			return i+1
 			// TODO: Delete from localqueue and tell queuemanager
 		}
@@ -39,7 +77,7 @@ func checkUp(start int, stop int, lockalQueue [][]bool) int {
 // Returns next floor ordered below current in DOWN queue or 0 if empty
 func checkDown(start int, stop int, lockalQueue [][]bool) int {
 	for i := floor; i >= stop; i--{
-		if localQueue[i][0] || localQueue[i][2]{
+		if localQueue[i][Down-1] || localQueue[i][Command-1]{
 			return i+1
 			// TODO: Delete from localqueue and tell queuemanager
 		}
