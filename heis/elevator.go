dd661e26bc50c91b19affca2598c68770b27497d
Inger Berge Hagen <ingerbergehagen@dhcp-008118.wlan.ntnu.no>
2014-03-09 17:37:23 +0100

    inew folder

--- heis/elevator.go
+++ heis/elevator.go
@@ -1,16 +1,17 @@
 package elevatorControl
+
 import (
 	"io"
 	"localQueue"
 )
 
 func elevatorControl(){
+	
 	floorOrder := make(chan uint)
 	buttonPress := make(chan Button)
 	floor := make(chan FloorStatus)
 	
 	init(floorOrder, floor)
-
 	go readButtons(buttonPress) // puts pressed buttons in the buttonPress chanel
 	
 	
--- heis/localQueue/localQueue.go
+++ heis/localQueue/localQueue.go
@@ -1,4 +1,5 @@
 package localQueue
+
 import (
 	"log"
 )
