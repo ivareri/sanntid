11289f8865f56f6659e4b467cd0f6a60c7ef37cd
Inger Berge Hagen <ingerbergehagen@dhcp-009225.wlan.ntnu.no>
2014-03-12 12:06:37 +0100

    matpause

--- heis/elevator.go
+++ heis/elevator.go
@@ -5,18 +5,40 @@ import (
 	"localQueue"
 )
 
+
 func elevatorControl(){
+	// localQueue indexing: [floor][0]=Up, [floor][1]=Down, [floor][2]=Command
+	// stupid way to init
+	// should it be a type instead?
+	var localQueue := [4][3]bool {{false, false, false},{false, false, false},{false, false, false},{false, false, false}}
+	
+	//Check file if there is an existing queue
+	
+	
 	floorOrder := make(chan uint)
 	buttonPress := make(chan Button)
 	floor := make(chan FloorStatus)
-	
-	// [floor][0]=up, [floor][1]=down, [floor][2]= command 
-	// localqueue[4][3] bool
-	
+
 	init(floorOrder, floor)
+
 	go ReadButtons(buttonPress) // puts pressed buttons in the buttonPress chanel
 	go GetOrder(floorOrder)
-	
+
 	// func that gets something from manager and runs the different localQueue funcs
+
+	
+	
+	for{
+		select{
+		case buttonPressed := <- buttonPress:
+			if buttonPressed.button == Up || buttonPressed.button == Down {
+				// to network or take self
+			} else if buttonPressed.button == Command {
+				addLockalCommand(buttonPressed, localQueue)
+			}
+			
+		}
+	}
 	
 }
+
--- heis/localQueue/localQueue.go
+++ heis/localQueue/localQueue.go
@@ -2,20 +2,22 @@ package localQueue
 
 import (
 	"log"
+	"encoding/json"
+	"os"
 )
 
 // Adds floor to local Queue and tells network
 func addLocalCommand(buttonPressed button, localQueue [][]bool){
 	SetLight(Light{buttonPressed.Floor, Command, true})
 	localQueue[floor-1][Command] = true 
-	//TODO: Tell network to addCommand(floor, lift)
+	//TODO: Skriv til fil
 }
 
 // Deletes floor from local Queue and tells network
 func deleteLocalCommand(floor uint ,localQueue [][]bool){
 	SetLight(Light{floor, Command, false})
 	localQueue[floor-1][Command] = false
-	//TODO: Tell network to deleteCommand(floor, lift)
+	//TODO: skriv til fil
 }
 
 // Adds request to localQueue
@@ -39,40 +41,38 @@ func deleteLocalRequest(Direction bool, floor uint, localQueue [][]bool){
 }
 
 // Returns next floor ordered from the local queue or 0 if empty
-func GetOrder(floorOrder chan uint status chan struct, localQueue [][]bool) int {
-	status := <-status
+func GetOrder(floorOrder chan uint, status chan FloorStatus, localQueue [][]bool){
+	status := <- status
 	currentFloor := status.floor
 	currentIndex = int(currentFloor-1)
 	
 	if status.direction {
 		if next := checkUp(currentIndex , 3, localQueue); next && currentIndex !=3 {
 			floorOrder <- next
-		}else if next := checkDown(3, 0, localQueue); next{
+		} else if next := checkDown(3, 0, localQueue); next {
 			floorOrder <- next
-		}else{
+		} else {
 			floorOrder <- checkUp(0, currentIndex, localQueue)
 			}
-		}
-	}else{
+	} else {
 		if next := checkDown(currentIndex , 0, localQueue); next && currentIndex !=3 {
 			floorOrder <- next
-		}else if next := checkUp(0, 3, localQueue); next{
+		} else if next := checkUp(0, 3, localQueue); next{
 			floorOrder <- next
-		}else{
+		} else {
 			floorOrder <- checkDown(3, currentIndex, localQueue)
-			}
 		}
 	}
 }
 
 // Returns next floor ordered above current in UP queue or 0 if empty
 func checkUp(start int, stop int, lockalQueue [][]bool) int {
-	for i := floor; i <= stop ; i++ {
-		if localQueue[i][Up-1] || localQueue[i][Command-1]{
+	for i := start; i <= stop ; i++ {
+		if localQueue[i][Up-1] || localQueue[i][Command-1] {
 			return i+1
-			// TODO: tell queuemanager?
 		}
-	} return 0
+	} 
+	return 0
 }
 
 // Returns next floor ordered below current in DOWN queue or 0 if empty
@@ -80,7 +80,24 @@ func checkDown(start int, stop int, lockalQueue [][]bool) int {
 	for i := floor; i >= stop; i--{
 		if localQueue[i][Down-1] || localQueue[i][Command-1]{
 			return i+1
-			// TODO: tell queuemanager?
 		}
-	} return 0
+	} 
+	return 0
+}
+
+func writeQueueToFile(localQueue [][]bool, filename string){
+	queue, _ := json.Marshal(localQueue [][]bool)
+	file, err := os.Create("localQueue")
+	if err != nil {
+		log.Println(err)
+	} 
+	n, err := file.Write(queue)
+	if err != nil {
+		log.Println(err)
+	}
+	
+}
+
+func readQueueToFile(filename string) [][]bool{
+	
 }
