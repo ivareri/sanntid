466cfbbae12aa6ab4015ff24090e854c50195d0c
Ivar Eriksen <ivar@flyndre.com>
2014-03-12 11:37:46 +0100

    Network done

--- heis/test/net.go
+++ heis/test/net.go
@@ -10,4 +10,14 @@ func main () {
 		fmt.Println("Error", err)
 	}
 	fmt.Println(liftnet.FindID(a))
+	send := make(chan liftnet.Message)
+	recieved := make(chan liftnet.Message)
+	liftnet.MulticastInit(send, recieved)
+	var bla liftnet.Message
+	bla.Id = 1
+	bla.Floor = 2
+	bla.Direction = false
+	bla.Status = liftnet.New
+	send <- bla
+	<-recieved
 }
