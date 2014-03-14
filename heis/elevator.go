8e2fea8dc3c8731f29df457e2f87dfe61ce09a1f
Inger Berge Hagen <ingerbergehagen@dhcp-009074.wlan.ntnu.no>
2014-03-14 19:50:34 +0100

    writing to file, soon

--- heis/elevator.go
+++ heis/elevator.go
@@ -3,42 +3,60 @@ package elevatorControl
 import (
 	"io"
 	"localQueue"
+	"time"
+	"io/ioutil"
 )
 
+type Queue struct {
+	upQueue [4]bool
+	downQueue [4]bool
+	commandQueue [4]bool
+}
 
 func elevatorControl(){
-	// localQueue indexing: [floor][0]=Up, [floor][1]=Down, [floor][2]=Command
-	// stupid way to init
-	// should it be a type instead?
-	var localQueue := [4][3]bool {{false, false, false},{false, false, false},{false, false, false},{false, false, false}}
-	
-	//Check file if there is an existing queue
-	
-	
-	floorOrder := make(chan uint)
-	buttonPress := make(chan Button)
-	floor := make(chan FloorStatus)
+	localQueue := Queue{}
 
-	init(floorOrder, floor)
+	//Check if there is an existing queue on file
+	
+	
+	floorOrder := make(chan uint) 			// channeling floor orders to io 
+	buttonPress := make(chan Button)		// channeling button presses from io
+	status := make(chan FloorStatus)		// channeling the lifts status
+	sendToNet := make(chan Message)			// channeling messages to the network
+	recieveFromNet := make(chan Message)	// channeling mesages to the network
 
-	go ReadButtons(buttonPress) // puts pressed buttons in the buttonPress chanel
+	init(floorOrder, status)			
+
+	go ReadButtons(buttonPress) 		
 	go GetOrder(floorOrder)
-
-	// func that gets something from manager and runs the different localQueue funcs
-
-	
 	
 	for{
 		select{
 		case buttonPressed := <- buttonPress:
 			if buttonPressed.button == Up || buttonPressed.button == Down {
+				log.Println("Request button %v pressed.", buttonPressed.button)
 				// to network or take self
 			} else if buttonPressed.button == Command {
+				log.Println("Command button %v pressed.", buttonPressed.Floor)
 				addLockalCommand(buttonPressed, localQueue)
+			} else if buttonPressed.button == Stop {
+				log.Println("Stop button pressed")
+				EmergencyStop(true)
+				// do or check something
+			} else if buttonPressed.button == Obstuction {
+				//does this even belong here ?
 			}
-			
+		default
+			log.Println("No buttons pressed.")	
 		}
 	}
 	
 }
+
+// timer thingy
+// now := time.Now()
+// diff := now.sub(then)
+// sum := then.add(diff) 
+// diff.Hours() osv -> Nanoseconds
+
 
