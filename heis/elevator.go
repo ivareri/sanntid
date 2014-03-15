a3c350ee3b6167f4e9f5a75e59a2622d799cc965
Inger Berge Hagen <ingerbergehagen@dhcp-010102.wlan.ntnu.no>
2014-03-15 15:37:46 +0100

    loerdagsjobbing

--- heis/elevator.go
+++ heis/elevator.go
@@ -1,38 +1,41 @@
 package elevatorControl
 
 import (
-	"io"
+	"liftio"
 	"localQueue"
 	"time"
-	"io/ioutil"
+	"log"
+	"os"
 )
 
+// change names to avoid confusion with buttons?
 type Queue struct {
-	upQueue [4]bool
-	downQueue [4]bool
-	commandQueue [4]bool
+	Up [4]bool
+	Down [4]bool
+	Command [4]bool
 }
 
 func elevatorControl(){
 	localQueue := Queue{}
-
-	//Check if there is an existing queue on file
-	
-	
+	ReadQueueFromFile(localQueue) 			// If no previous queue:
+											// logs error: "queue.txt doesn't exitst" 
+		
 	floorOrder := make(chan uint) 			// channeling floor orders to io 
 	buttonPress := make(chan Button)		// channeling button presses from io
 	status := make(chan FloorStatus)		// channeling the lifts status
-	sendToNet := make(chan Message)			// channeling messages to the network
-	recieveFromNet := make(chan Message)	// channeling mesages to the network
+	// Rename to LiftStatus?
+	toNetwork := make(chan Message)				// channeling messages to the network
+	fromNetwork := make(chan Message)	  		// channeling messages to the network
 
-	init(floorOrder, status)			
+	init(floorOrder, status)
+	MultiCastInit(toNetwork, fromNetwork)			
 
 	go ReadButtons(buttonPress) 		
 	go GetOrder(floorOrder)
 	
 	for{
 		select{
-		case buttonPressed := <- buttonPress:
+		case buttonPressed := <-buttonPress:
 			if buttonPressed.button == Up || buttonPressed.button == Down {
 				log.Println("Request button %v pressed.", buttonPressed.button)
 				// to network or take self
@@ -46,11 +49,36 @@ func elevatorControl(){
 			} else if buttonPressed.button == Obstuction {
 				//does this even belong here ?
 			}
-		default
-			log.Println("No buttons pressed.")	
+		case request := <-fromNet:
+			FS := figureOfSuitability(request,)
+			
+		default:
+			log.Println("No buttons pressed.")
+			// teit å skrive  ut hele tiden?	
 		}
 	}
 	
+}
+
+func assignLift(toNetwork ){
+	toNetwork <- jhk
+	
+}
+
+// Lift with largest FS should accept the request 
+func figureOfSuitability(request Message, status FloorStatus) int {
+	reqDir := request.Direction
+	reqFlr := request.Floor
+	statDir := status.Direction
+	statFlr := status.Floor
+	if reqDir==Up && statDir==up && reqFlr > statFlr {
+		FS := MAXFLOOR + 1 - diff(reqFlr,statFlr)
+	} else if  reqDir==Down && statDir==up && requFlr < statFlr {
+		FS := MAXFLOOR - diff(reqFlr,statFlr)
+	} else {
+		FS := 1
+	}
+	return FS
 }
 
 // timer thingy
