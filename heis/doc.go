3bda8120d966263e0263388bd10500b2a1a1514d
Ivar Eriksen <ivar@flyndre.com>
2014-03-19 15:24:59 +0100

    Added todo

---
+++ heis/doc.go
@@ -0,0 +1,19 @@
+// TODO:
+// main.go:
+// Add watchdog. Restart lift if main process dies. (As in ex6)
+//
+// Network:
+// UDP-multicast not guaranteed delivery. 
+// Possible fixes:
+// -- While not confirmed by any other elevator: save to file
+// -- Have elevators reply as they get messages. (should still be less networkoverhead compared to unicast
+//
+// Priority:
+// Relaclucate FS and check if order should be moved to other lifts.
+// Need new status field to transmitt over network.
+// Need abillity to remove order from localQueue. 
+//(localQueue.DeleteLocalOrder deletes commands, so not that one)
+//
+// Consitency in language
+// lift/elevator used multiple places.
+// order/request/command might not be consistent for all packages.
